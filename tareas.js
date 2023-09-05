8ce33017b24a602f2695d14bff809e9ad33e888a
Jose Sosa <josesosa535@gmail.com>
2023-09-05 09:46:49 -0500

    Servidor Lista de Tareas Express Router

---
+++ tareas.js
@@ -0,0 +1,12 @@
+// Definir la lista de tareas
+const tasks = [
+  { id: 1, descripcion: "Organizar menu del restaurante", estado: "pendiente" },
+  {
+    id: 2,
+    descripcion: "Revisar las tareas de la semana",
+    estado: "completado",
+  },
+  { id: 3, descripcion: "Estudiar API's", estado: "pendiente" },
+];
+
+module.exports = tasks;
